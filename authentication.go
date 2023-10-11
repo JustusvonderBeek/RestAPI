140c4e9b9fed282fb2a130b5f2ba9ea7a1ce77da
Justus von der Beek <justusvonderbeek@gmail.com>
2023-10-11 18:46:56 +0200

    Fixed regex group

--- authentication.go
+++ authentication.go
@@ -34,9 +34,11 @@ func IPWhiteList(whitelist map[string]bool) gin.HandlerFunc {
 	f := func(c *gin.Context) {
 		// If the IP isn't in the whitelist, forbid the request.
 		ip := c.ClientIP()
+		// log.Printf("IP: %s", ip)
 
-		re := regexp.MustCompile(`[\d]+.[\d]+.[\d]+.[\d]+`)
+		re := regexp.MustCompile(`([\d]+).([\d]+).[\d]+.[\d]+`)
 		ipRange := re.ReplaceAllString(ip, "$1.$2.0.0")
+		// log.Printf("Ip Range: %s", ipRange)
 		if !whitelist[ip] && !whitelist[ipRange] {
 			log.Printf("Unauthorized access from %s", ip)
 			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "fuck you in the ass blyad"})
